cfa0832cafce937f3380056ea68f3c3cca46e70a
shaff600 <shafyl@live.co.uk>
2021-09-18 16:23:41 +0000

    added surplus data to worksheet

--- run.py
+++ run.py
@@ -84,6 +84,16 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def update_surplus_worksheet(data):
+    """
+    Store surplus values in google spreadsheet
+    """
+    print("Updating surplus values")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully")
+
+
 
 def main():
     """
@@ -93,6 +103,8 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
+    
     print(new_surplus_data)
 
 
